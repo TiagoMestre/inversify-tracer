ef54d87f77b9effe67b43571ad92420f6ffcc9b9
Tiago Fernandes <tiagomestre91@gmail.com>
2016-11-06 18:31:47 +0000

    fix lint in filter/constants

--- src/filters/constants.ts
+++ src/filters/constants.ts
@@ -3,4 +3,3 @@ export const filterRegex: RegExp = /^(!?[A-Z|a-z|0-9|\_|\$|\*]+):?([A-Z|a-z|0-9|
 
 export const includeRegex: RegExp = /^[A-Z|a-z|0-9|\_|\$|\*]+:[A-Z|a-z|0-9|\_|\$|\*]+$/;
 export const excludeRegex: RegExp = /^![A-Z|a-z|0-9|\_|\$|\*]+:[A-Z|a-z|0-9|\_|\$|\*]+$/;
-
