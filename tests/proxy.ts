9638ee1a13bdae8842368ec1fe37fc274ea63ce2
Tiago Fernandes <tiagomestre91@gmail.com>
2016-11-11 00:40:15 +0000

    removed extra code and fixed lint errors

--- tests/proxy.ts
+++ tests/proxy.ts
@@ -9,7 +9,7 @@ import { MethodFilter } from './../src/filters';
 class TestObject {
 	public method() {}
 	public methodWithValue(value: any) { return value; }
-	public methodPromiseResolve() { return Promise.resolve() }
+	public methodPromiseResolve() { return Promise.resolve(); }
 	public methodPromiseResolveWithValue(value: any) { return Promise.resolve(value); }
 	public methodPromiseRejectWithValue(value: any) { return Promise.reject(value); }
 }
